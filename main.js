912c5b15b685e22e11e1bc338f0c75c2536309c5
Rushet <140152786+rushet@users.noreply.github.com>
2023-08-14 16:39:19 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-let mountains = ['Panjab', 'Fuji', 'K3' , 'K4'];
+let mountains = ['Panjab', 'Fuji', 'K3' , 'K4', 'K5'];
 
 console.log(mountains[0]); 
 console.log(mountains[1]); 
 console.log(mountains[2]); 
 console.log(mountains[3]); 
+console.log(mountains[4]); 
